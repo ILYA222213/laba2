57e8abb8edd56733bc1f6910beca9ec598edbca3
Сом <117897751+ILYA222213@users.noreply.github.com>
2023-09-14 12:21:49 +0300

    Add files via upload

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Получаем все текущие процессы
+        Process[] processes = Process.GetProcesses();
+
+        Console.WriteLine("Все процессы:");
+        Console.WriteLine("==============");
+
+        // Выводим PID и имя всех процессов
+        foreach (Process process in processes)
+        {
+            Console.WriteLine($"PID: {process.Id}, Имя: {process.ProcessName}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Первые три процесса:");
+        Console.WriteLine("==================");
+
+        // Выводим первые три процесса
+        for (int i = 0; i < 3; i++)
+        {
+            Process process = processes[i];
+            Console.WriteLine($"PID: {process.Id}, Имя: {process.ProcessName}");
+        }
+    }
+}
